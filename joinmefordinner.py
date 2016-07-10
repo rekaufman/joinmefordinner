e6b743bb85215d0a66acddac092fa2a5aa40bac1
Tavi Nathanson <tavi.nathanson@gmail.com>
2016-07-10 16:36:44 -0400

    Add tiramisu!

--- joinmefordinner.py
+++ joinmefordinner.py
@@ -12,7 +12,8 @@ class FakeDatabase(object):
         post_3 = ("Pretzels", "Boil them!")
         post_4 = ("French fries", "Fry them!")
         post_5 = ("Mushroom soup", "Simmer it")
-        posts = [post_1, post_2, post_3, post_4, post_5]
+        post_6 = ("Tiramisu", "Prepare it!")
+        posts = [post_1, post_2, post_3, post_4, post_5, post_6]
         return choice(posts)
 
 @app.route("/")
